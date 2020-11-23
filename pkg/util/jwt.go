5f6def21f3f53340ee12a7aef5b7ab607bcb753e
starbuling-l <wo1137463141@gmail.com>
2020-11-23 20:04:07 +0800

    add jwt

--- pkg/util/jwt.go
+++ pkg/util/jwt.go
@@ -1,10 +1,16 @@
 package util
 
-import "github.com/starbuling-l/StarBlog/pkg/setting"
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/starbuling-l/StarBlog/pkg/setting"
+)
 
 var jwtSecret = []byte(setting.JwtSecret)
 
 type Claims struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+	jwt.StandardClaims
 }
+
+
